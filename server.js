cdecd414e1ae34dbb974b527938b280bfc59717f
anacarol-1808 <126124189+anacarol-1808@users.noreply.github.com>
2024-11-09 22:26:03 -0700

    connected to mongodb

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const mongodb = require('./data/database')
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes/index'));
 
-app.listen(process.env.PORT || port, () => {
-    console.log('Web Server is listening at port ' + (process.env.PORT || port));
-  });
+mongodb.initDb((err) => {
+    if(err) {
+        console.log(err);
+    }
+    else{
+        app.listen(process.env.PORT || port, () => {
+            console.log('Database is listening and node running at port ' + (process.env.PORT || port));
+          });
+    }
+})
+
