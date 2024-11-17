dbd71e6649a2e20c558fa6a88444a568a1c1fca7
anacarol-1808 <126124189+anacarol-1808@users.noreply.github.com>
2024-11-16 21:43:39 -0700

    week 02: Project 1 PUT, POST, DELETE routes implemented and server runs

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const usersController = require('../controllers/contacts');
+const usersController = require('../controllers/users');
 
 router.get('/', usersController.getAll);
 router.get('/:id', usersController.getSingle);
+
+// week 02 - Add API routes to perform GET, POST, PUT, and DELETE requests
+router.post('/', usersController.createUser);
+router.put('/:id', usersController.updateUser);
+router.delete('/:id', usersController.deleteUser);
 
 module.exports = router;
